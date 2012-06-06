210f7e9792ca82ec2fb07f42bb4fbdbe10b1ef64
Eric Jones <erjjones@gmail.com>
2012-06-05 23:38:51 -0400

    User flow

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,6 @@ class Ingredient < ActiveRecord::Base
   attr_accessible :description, :measure, :unit
   validates :description,  :presence => true, :length => { :minimum => 3 }  
   validates :unit,  :presence => true, :length => { :minimum => 1 }
+  
+    MEASURE = ['dash(s)','cup(s)','pint(s)','T(s)','tsp(s)','can(s)','box(s)','container(s)','gallon(s)','quart(s)','oz(s)','slice(s)']
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,10 @@ class Recipe < ActiveRecord::Base
   validates :name,  :presence => true, :length => { :minimum => 3 }
   validates :author, :presence => true, :length => { :minimum => 3 }
   validates :category, :presence => true, :length => { :minimum => 3 }
-  validates :instructions, :presence => true, :length => { :minimum => 3 }
   
   has_many :ingredients, :dependent => :destroy
+  
+  CATEGORIES = ['Appetizers', 'Beverages', 'Breads', 'Dessert',  'Main Dish', 'Salad', 'Side Dish', 'Soup', 'Vegetables']
+
 end
 
